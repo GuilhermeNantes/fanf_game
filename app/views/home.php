6beec97ca16f251494e9e9ce039a8c4d659f6a03
GuilhermeNantes <guinantes27@gmail.com>
2025-02-26 17:12:59 -0400

    home inical

---
+++ app/views/home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Courier New', Courier, monospace;
+        }
+        .logo img,.perfil img{
+            display: flex; 
+            height: 50px;
+            width: 50px;
+            border-radius: 5px;
+        }
+
+        
+    </style>
+    <header>
+        <nav>
+            <a href='#'class="logo"><img src="http://localhost/host-gui/fanf_game/public/image/15074001_5552120.jpg" alt="Logo"></a>
+            <ul>
+                <li><a href="">inicio</a></li>
+                <li><a href="">estudos</a></li>
+                <li><a href="">sobre-nos</a></li>
+            </ul>
+            <div class="perfil"><img src="http://localhost/host-gui/fanf_game/public/image/images.jpg" alt="Img do usario o perfil"></div>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <div>
+                <h1>
+                    oi teste
+                </h1>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
